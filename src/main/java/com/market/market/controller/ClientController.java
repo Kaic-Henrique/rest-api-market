b14836f1a84f31e1ffb9825fbc3f6e905d239f08
Kaic Henrique <kaicsena31@gmail.com>
2023-08-14 17:44:54 -0300

    add controllers

--- src/main/java/com/market/market/controller/ClientController.java
+++ src/main/java/com/market/market/controller/ClientController.java
@@ -1,2 +1,42 @@
-package com.market.market.controller;public class ClientController {
+package com.market.market.controller;
+
+import com.market.market.entity.ClientEntity;
+import com.market.market.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final ClientRepository clientRepository;
+
+    @GetMapping("/Clients")
+    public ResponseEntity<List<ClientEntity>> findAll(){
+        List<ClientEntity> findClients = clientRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(findClients);
+    }
+
+    @GetMapping("/client/{id}")
+    public  ResponseEntity<ClientEntity> findById(@PathVariable Long id){
+        ClientEntity findClient = clientRepository.findById(id).get();
+        return ResponseEntity.status(HttpStatus.OK).body(findClient);
+    }
+
+    @PostMapping("/registerClient")
+    public ResponseEntity<ClientEntity> save(@RequestBody ClientEntity clientEntity){
+        ClientEntity registerClient = clientRepository.save(clientEntity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerClient);
+    }
+
+    @DeleteMapping("/deleteRegister/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        clientRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- src/main/java/com/market/market/controller/ProductController.java
+++ src/main/java/com/market/market/controller/ProductController.java
@@ -1,2 +1,43 @@
-package com.market.market.controller;public class ProductController {
+package com.market.market.controller;
+
+
+import com.market.market.entity.ProductEntity;
+import com.market.market.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductEntity>> findAll(){
+        List<ProductEntity> products = productRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductEntity> findById(@PathVariable Long id){
+        ProductEntity findProduct = productRepository.findById(id).get();
+        return ResponseEntity.status(HttpStatus.OK).body(findProduct);
+    }
+
+    @PostMapping("/registerProduct")
+    public ResponseEntity<ProductEntity> save(@RequestBody ProductEntity productEntity){
+        ProductEntity registerProduct = productRepository.save(productEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(registerProduct);
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        productRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
